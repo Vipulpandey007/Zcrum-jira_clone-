7b8b7eaafbe1dbc7434ebab55a3b5279bb6460ab
Vipulpandey007 <vipulpandey708@gmail.com>
2024-11-15 13:40:47 +0530

    header created

--- middleware.js
+++ middleware.js
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/onboarding(.*)",
+  "/organisation(.*)",
+  "/project(.*)",
+  "/issue(.*)",
+  "/sprint(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
